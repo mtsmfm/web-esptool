86b81a3b04a74309c67359855e1150243e6b485d
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2022-06-20 11:19:27 +0000

    Remove vue stuff

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-
-import App from './App.vue';
-
-createApp(App).use(Antd).mount('#app');
